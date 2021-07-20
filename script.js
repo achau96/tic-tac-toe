ee6e07ac84b2f38d012ed4228221b04234d5ac5d
Austin Chau <austin.chau1@gmail.com>
2021-07-20 14:48:46 -0400

    working win condition in script

--- script.js
+++ script.js
@@ -33,18 +33,43 @@ const Player = () => {
 }
 
 const game = ((Player)=>{
+  const winConditions = [
+    [0,1,2], //rows
+    [3,4,5],
+    [6,7,8],
+    [0,3,6], //columns
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],//diagonials
+    [2,4,6]
+  ];
+
+  const checkWin = () => {
+    let winArray = [];
+    for(let i = 0; i<winConditions.length;i++){
+      winConditions[i].forEach((index,j) => {
+        winArray[j] = gameBoard.board[index];
+      })
+      console.log(winArray);
+      if(winArray[0]!='' && winArray[0]==winArray[1] && winArray[1] == winArray[2]){
+        console.log(`${winArray[0]} wins!`);
+      }
+    }
+  }
+
   let turn='X';
   const buttons = document.querySelectorAll('.xo');
   const start = () => buttons.forEach((button,index) => button.addEventListener('click',
   ()=>{
     Player().state(index,turn);
+    checkWin();
     if(turn == 'X'){turn = 'O'}
     else {turn = 'X'}
   },{
   once:true //removes event listener once triggered
   }));
 
-  return {start};
+  return {start}
 })(Player);
 
 game.start();
